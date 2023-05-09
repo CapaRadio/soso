e84ea68b886dd70bcf04013b7ce78cb63a6d248f
CapaRadio <dariocapalbo94@gmail.com>
2023-05-09 22:53:26 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+console.log("Hello");
+
+
+function UR_Start()
+{
+UR_Nu = new Date();
+UR_Indhold = showFilled(UR_Nu.getHours())+ ":" + showFilled(UR_Nu.getMinutes()) + ":" + showFilled(UR_Nu.getSeconds());
+document.all.heure.innerHTML = UR_Indhold;
+//document.writeln(UR_Indhold);
+setTimeout("UR_Start()",1000);
+}
+function showFilled(Value)
+{
+return (Value > 9) ? "" + Value : "0" + Value;
+}
